8cdaa17ebf43e52ae14f62edd5ca636f69c84988
basra4ka <evgeha_ivanov_01@bk.ru>
2024-09-30 20:13:58 +0300

    plan

--- src/main/kotlin/transportTaskLab/TableClass.kt
+++ src/main/kotlin/transportTaskLab/TableClass.kt
@@ -1,24 +1,38 @@
 package transportTaskLab
 
+import java.lang.Integer.min
+
+
 data class TableClass(
-    private var froms: MutableList<String>,
-    private var tos: MutableList<String>,
-    private var tariffMatrix: MutableList<MutableList<Int>>
+    private val froms: MutableList<String>,
+    private val tos: MutableList<String>,
+    private val tariffMatrix: List<List<Int>>,
+    private val stocks: List<Int>,
+    private val needs: List<Int>
 ) {
+
+    private val stockWord = "Запасы"
+    private val needsWord = "Потребность"
+    private val stocksAndNeedsSum by lazy { stocks.sum() }
+
+    val planMatrix: MutableList<MutableList<Int>> by lazy{
+        MutableList(froms.size){MutableList(tos.size){0} }
+    }
 
     init {
         require(isFit(froms.size, tos.size, tariffMatrix))
+        require(stocks.sum() == needs.sum())
     }
 
     fun printData(){
         val lengths = mutableListOf<Int>()
         val tosMaxLength = mutableMapOf<Int, Int>()
-        tos.forEachIndexed {i, it ->
+        (tos + mutableListOf(stockWord)).forEachIndexed { i, it ->
             lengths.add(it.length)
             tosMaxLength[i] = it.length
         }
 
-        val fromsMaxLength = froms.maxBy { it.length }.length
+        val fromsMaxLength = (froms + mutableListOf(needsWord)).maxBy { it.length }.length
 
         val length = lengths.fold(fromsMaxLength) { acc, len ->
             acc + (len + 2) }
@@ -29,19 +43,103 @@ data class TableClass(
         for(to in tos){
             print("$to |")
         }
+        print(stockWord)
 
         println()
         printRepeatedly("=", length = length)
 
-        for(i in 0..<froms.size){
+
+        for(i in tariffMatrix.indices){
             print("${froms[i]} ${" ".repeat(fromsMaxLength - froms[i].length)}|")
 
-            for(j in tariffMatrix.indices){
-                print("${tariffMatrix[j][i]} ${" ".repeat(tosMaxLength[j]!! - tariffMatrix[j][i].toString().length)}|")
+            tariffMatrix[i].forEachIndexed{ id, it ->
+                print("$it ${" ".repeat(tosMaxLength[id]!! - it.toString().length)}|")
             }
+
+            print("${stocks[i]} ${" ".repeat(tosMaxLength[tos.lastIndex+1]!! - stocks[i].toString().length)}")
+
             println()
             printRepeatedly("=", length = length)
         }
+
+        print("$needsWord ${" ".repeat(fromsMaxLength - needsWord.length)}|")
+
+        for(needsId in needs.indices){
+            print("${needs[needsId]} ${" ".repeat(tosMaxLength[needsId]!! - needs[needsId].toString().length)}|")
+        }
+        print(stocksAndNeedsSum)
+    }
+
+    fun createInitialPlan(){
+        val tempNeeds = needs.toMutableList()
+        val tempStocks = stocks.toMutableList()
+        var needsId = 0
+        var stocksId = 0
+
+        while(needsId < tempNeeds.size && stocksId < tempStocks.size){
+            val minimalVal = min(tempNeeds[needsId], tempStocks[stocksId])
+
+            tempNeeds[needsId] -= minimalVal
+            tempStocks[stocksId] -= minimalVal
+
+            planMatrix[needsId][stocksId] = minimalVal
+
+            if(tempNeeds[needsId] == 0) needsId++
+            if(tempStocks[stocksId] == 0) stocksId++
+        }
+
+        planMatrix.forEach {
+            println(it.joinToString())
+        }
+
+        println(tempNeeds.joinToString())
+        println(tempStocks.joinToString())
+    }
+
+    fun printPlan(){
+        val lengths = mutableListOf<Int>()
+        val tosMaxLength = mutableMapOf<Int, Int>()
+        (tos + mutableListOf(stockWord)).forEachIndexed { i, it ->
+            lengths.add(it.length)
+            tosMaxLength[i] = it.length
+        }
+
+        val fromsMaxLength = (froms + mutableListOf(needsWord)).maxBy { it.length }.length
+
+        val length = lengths.fold(fromsMaxLength) { acc, len ->
+            acc + (len + 2) }
+
+        printRepeatedly("=", length = length)
+        printRepeatedly(" ", length = fromsMaxLength+1, after = "|", newLine = false)
+
+        for(to in tos){
+            print("$to |")
+        }
+        print(stockWord)
+
+        println()
+        printRepeatedly("=", length = length)
+
+
+        for(i in tariffMatrix.indices){
+            print("${froms[i]} ${" ".repeat(fromsMaxLength - froms[i].length)}|")
+
+            tariffMatrix[i].forEachIndexed{ id, it ->
+                print("$it(${planMatrix[i][id]}) ${" ".repeat(tosMaxLength[id]!! - it.toString().length)}|")
+            }
+
+            print("${stocks[i]} ${" ".repeat(tosMaxLength[tos.lastIndex+1]!! - stocks[i].toString().length)}")
+
+            println()
+            printRepeatedly("=", length = length)
+        }
+
+        print("$needsWord ${" ".repeat(fromsMaxLength - needsWord.length)}|")
+
+        for(needsId in needs.indices){
+            print("${needs[needsId]} ${" ".repeat(tosMaxLength[needsId]!! - needs[needsId].toString().length)}|")
+        }
+        print(stocksAndNeedsSum)
     }
 
     private fun printRepeatedly(str:String, length:Int, newLine:Boolean = true,
@@ -50,7 +148,7 @@ data class TableClass(
         else print("$before${str.repeat(length)}$after")
     }
 
-    private fun isFit(m:Int, n:Int, matrix:MutableList<MutableList<Int>>):Boolean{
+    private fun isFit(m:Int, n:Int, matrix:List<List<Int>>):Boolean{
         var isFit = true
         if(m == matrix.size){
             for (lst in matrix){
--- src/main/kotlin/transportTaskLab/TransportTaskMain.kt
+++ src/main/kotlin/transportTaskLab/TransportTaskMain.kt
@@ -2,15 +2,22 @@ package transportTaskLab
 
 fun main(){
 
-    val tc = TableClass(mutableListOf("Омск", "Новосибирск", "Томск", "Потребность"),
-        mutableListOf("Н. Новгород", "Пермь", "Краснодар", "Запасы"),
-        mutableListOf(
-            mutableListOf(4668, 5081, 5727, 2000),
-            mutableListOf(4083, 4097, 4798, 1000),
-            mutableListOf(5925, 5902, 6888, 2300),
-            mutableListOf(2000, 1700, 1600, 5300)
+    val tc = TableClass(
+        froms = mutableListOf("Омск", "Новосибирск", "Томск"),
+        tos = mutableListOf("Н. Новгород", "Пермь", "Краснодар"),
+        stocks = listOf(2000, 1700, 1600),
+        needs = listOf(2000, 1000, 2300),
+        tariffMatrix = listOf(
+            listOf(4668, 4083, 5925),
+            listOf(5081, 4097, 5902),
+            listOf(5727, 4798, 6888),
         )
     )
 
     tc.printData()
+
+    println()
+    tc.createInitialPlan()
+
+    tc.printPlan()
 }

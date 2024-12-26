22cd20b76d44e3458d8b6eaad58f8b8e9eee9e9b
basra4ka <evgeha_ivanov_01@bk.ru>
2024-12-26 20:23:31 +0300

    empty

---
+++ src/main/kotlin/model_systems/game_theory/GT_Lab2.kt
@@ -0,0 +1,14 @@
+package model_systems.game_theory
+
+
+
+
+fun main(){
+    val a = arrayOf(
+        intArrayOf(2, 6, 4, 2),
+        intArrayOf(7, 2, 3, 1),
+        intArrayOf(5, 3, 7, 5)
+    )
+
+
+}

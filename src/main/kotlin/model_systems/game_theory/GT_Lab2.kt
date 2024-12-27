c092a1f5d516974743fff895ba5865a52e209dd1
basra4ka <evgeha_ivanov_01@bk.ru>
2024-12-27 21:44:35 +0300

    empty

--- src/main/kotlin/model_systems/game_theory/GT_Lab2.kt
+++ src/main/kotlin/model_systems/game_theory/GT_Lab2.kt
@@ -1,6 +1,36 @@
 package model_systems.game_theory
 
+fun matrixConv(a:Array<IntArray>):Array<IntArray>?{
 
+    fun findDominantRows():MutableList<Int>{
+        val isRowDominant: (IntArray, IntArray) -> Boolean = { row1, row2 ->
+            row1.zip(row2).all { (a, b) -> a >= b }
+        }
+
+        val domRows = mutableListOf<Int>()
+
+        for(i in a.indices){
+            var isDom = true
+            for(j in a.indices){
+                if(i != j  && !isRowDominant(a[i], a[j])){
+                    isDom = false
+                    break
+                }
+            }
+
+            if(isDom){
+                domRows.add(i)
+                if(domRows.size == 2) break
+            }
+        }
+
+        return domRows
+    }
+
+    val domr = findDominantRows()
+    println(domr.joinToString(" "))
+    return null
+}
 
 
 fun main(){
@@ -10,5 +40,6 @@ fun main(){
         intArrayOf(5, 3, 7, 5)
     )
 
+    matrixConv(a)
 
 }

4755298862591c716dbe9281527692767939c8cb
basra4ka <evgeha_ivanov_01@bk.ru>
2025-01-10 19:06:00 +0300

    sa

--- src/main/kotlin/model_operations/SPLab/LabClone.kt
+++ src/main/kotlin/model_operations/SPLab/LabClone.kt
@@ -8,7 +8,7 @@ fun main(){
         Event(2, 5, 11), Event(2, 6, 15),
         Event(3, 4, 14), Event(3, 6, 10),
         Event(4, 7, 19), Event(5, 8, 5),
-        Event(6, 8, 11), Event(6, 9, 12),
+        Event(6, 8, 11), Event(6, 9, 40),
         Event(7, 9, 16), Event(8, 9, 14))
 
     val net = Net(transitions, 9)
@@ -57,5 +57,4 @@ fun main(){
     criticalWay.forEach {
         println(it)
     }
-
 }
